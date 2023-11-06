b4fce1bde72b6fd725c406064e53e969ffa13588
sevincc7 <146056348+sevincc7@users.noreply.github.com>
2023-11-06 12:05:22 +0400

    Add files via upload

---
+++ jquern/assets/jquery.js
@@ -0,0 +1,33 @@
+
+// $("#btn").click(function(){
+// $("div").hide()
+// })
+// //hide-gizletmek
+// //show-gostermek
+// //toggle hide+show
+// $("#btn1").click(function(){
+//     $("div").show()
+// })
+
+// $("#btn2").click(function(){
+//     $("div").toggle(2000)
+// })
+
+// $("#btn").click(function(){
+//     $("#one").slideToggle()
+// })
+// $("#btn1").click(function(){
+//     $("#two").slideToggle()
+// })
+// $("#btn2").click(function(){
+//     $("#three").slideToggle()
+// })
+
+$("h1").mouseover(function(){
+    $(this).css("background-color","green")
+})
+
+//mouseleave
+$("h1").mouseleave(function(){
+    $(this).css("background-color", "red")
+})
